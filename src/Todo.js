038695cfe20c650599e0e1c608181d3f89d200a6
Massafelli <lmassafelli@yahoo.com.br>
2021-09-15 17:03:51 -0300

    Uploading
    Uploading files

---
+++ src/Todo.js
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from "react";
+import './Todo.css'
+import List from './components/List';
+import TodoForm from "./components/TodoForm";
+import Item from './components/Item';
+import Modal from "./components/Modal";
+
+const SAVED_ITEMS = 'savedItems'
+
+function Todo(){
+
+    const [items,setItems] = useState([]);
+    const [showModal, setShowModal] = useState(false);
+
+    useEffect(() => {
+        let savedItems = JSON.parse(localStorage.getItem(SAVED_ITEMS));
+
+        if(savedItems){
+            setItems(savedItems);
+        }
+    },[])
+
+    useEffect(() => {
+        localStorage.setItem(SAVED_ITEMS,JSON.stringify(items))
+    },[items])
+
+    function onAddItem(text){
+
+        let it = new Item(text);
+
+        setItems([...items,it])
+        onHideModal();
+    }
+
+    function onItemDeleted(item){
+        
+        let filteredItems = items.filter(it => it.id !== item.id);
+        setItems(filteredItems)
+    }
+
+    function onDone(item){
+        let updatedItems = items.map(it => {
+            if(it.id === item.id){
+                it.done = !it.done;
+            }
+
+            return it;
+        })
+
+        setItems(updatedItems);
+    }
+
+    function onHideModal(event){
+        setShowModal(false);
+    }
+    
+    return (
+        <div className='container'>
+            <header className='header'>
+            <h1>Todo</h1>
+            <button onClick={() => setShowModal(true)} className='addBtn'>+</button>
+            </header>
+            <List onDone={onDone} onItemDeleted={onItemDeleted} items={items}/>
+            <Modal onHideModal={onHideModal} show={showModal}>
+            <TodoForm onAddItem={onAddItem}/>
+            </Modal>
+        </div>
+    )
+}
+
+
+export default Todo;
